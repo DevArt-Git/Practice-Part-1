7e94e52f6b511e9f72f8103710771c5bc4d4d435
Artem <artem.chern@gmail.com>
2022-03-06 17:04:23 +0300

    Made an object

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+let numberOfFilms = +prompt('Сколько фильмов уже посмотрели', 'Хотя бы примерно');
+
+let personaMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+let a = prompt("Один из последних просмотренных фильмов?", ''),
+    b = prompt("На сколько оцените его?", '' ),
+    c = prompt("Один из последних просмотренных фильмов?", ''),
+    d = prompt("На сколько оцените его?", '' );
+
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+
+console.log(personaMovieDB);
